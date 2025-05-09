6536f375be64b31be2ee842f73296e9f4ad8ef75
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-09 09:59:47 -0300

    chore: estrutura inicial do projeto PollHub com pacotes

---
+++ src/main/java/com/pablorodriguesb/pollhub/PollHubApplication.java
@@ -0,0 +1,13 @@
+package com.pablorodriguesb.pollhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablorodriguesb/pollhub/PollHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablorodriguesb.pollhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
