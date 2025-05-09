bd6b95575f4ee9841e003f71787f59aaa26c80d3
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-09 10:22:39 -0300

    feat: adiciona entidade User

---
+++ src/main/java/com/pablorodriguesb/pollhub/model/User.java
@@ -0,0 +1,34 @@
+package com.pablorodriguesb.pollhub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // armazenar hash da senha
+
+    @Column(nullable = false)
+    private LocalDateTime dataCadastro;
+}
