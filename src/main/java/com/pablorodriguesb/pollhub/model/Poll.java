95eae9fc2df64a6f0676bd2d85b7d61a65bd008f
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-09 10:28:45 -0300

    feat: adiciona entidade Poll

---
+++ src/main/java/com/pablorodriguesb/pollhub/model/Poll.java
@@ -0,0 +1,44 @@
+package com.pablorodriguesb.pollhub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "polls")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @Column(nullable = false)
+    private Boolean isPublic;
+
+    @OneToMany(mappedBy = "poll", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Option> options;
+}
+
+
