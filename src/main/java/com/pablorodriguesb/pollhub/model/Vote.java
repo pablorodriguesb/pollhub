34dd5aeda59a831239cd5dd038620a91cb614927
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-09 10:35:39 -0300

    feat: adiciona entidade Vote

---
+++ src/main/java/com/pablorodriguesb/pollhub/model/Vote.java
@@ -0,0 +1,40 @@
+package com.pablorodriguesb.pollhub.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "poll_id"})
+    }
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id", nullable = false)
+    private Option option;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @Column(nullable = false)
+    private LocalDateTime votedAt;
+}
