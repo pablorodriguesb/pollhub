6946da9c88d5d3fe86c399fb7745548bb8d906be
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-10 12:34:40 -0300

    fix: corrige geracao e validacao de token JWT

--- src/main/java/com/pablorodriguesb/pollhub/security/JwtTokenUtil.java
+++ src/main/java/com/pablorodriguesb/pollhub/security/JwtTokenUtil.java
@@ -3,10 +3,12 @@ package com.pablorodriguesb.pollhub.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
@@ -33,7 +35,12 @@ public class JwtTokenUtil {
     }
 
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
         private Boolean isTokenExpired(String token) {
@@ -42,14 +49,15 @@ public class JwtTokenUtil {
     }
 
     public String generateToken(UserDetails userDetails) {
+        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() +
-                JWT_TOKEN_VALIDITY))
-                .signWith(SignatureAlgorithm.ES512, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
+
 
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
