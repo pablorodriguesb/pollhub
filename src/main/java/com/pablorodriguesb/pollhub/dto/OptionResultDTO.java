4a5d5f2a80d25f90594b0444258077b2485186ba
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-10 18:11:04 -0300

    feat: adiciona DTOs para retorno de resultados de enquetes

--- src/main/java/com/pablorodriguesb/pollhub/dto/OptionResultDTO.java
+++ src/main/java/com/pablorodriguesb/pollhub/dto/OptionResultDTO.java
@@ -1,4 +1,10 @@
 package com.pablorodriguesb.pollhub.dto;
 
+import lombok.Data;
+
+@Data
 public class OptionResultDTO {
+    private Long optionId;
+    private String text;
+    private int votes;
 }
--- src/main/java/com/pablorodriguesb/pollhub/dto/PollResultsDTO.java
+++ src/main/java/com/pablorodriguesb/pollhub/dto/PollResultsDTO.java
@@ -1,4 +1,11 @@
 package com.pablorodriguesb.pollhub.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class PollResultsDTO {
+    private Long pollId;
+    private List<OptionResultDTO> results;
 }
