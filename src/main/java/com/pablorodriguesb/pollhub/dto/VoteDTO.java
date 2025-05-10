ef738180297cfd6bb734a7f7894786d1edff951e
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-10 18:11:20 -0300

    refactor: ajusta VoteDTO para uso no endpoint de votação

--- src/main/java/com/pablorodriguesb/pollhub/dto/VoteDTO.java
+++ src/main/java/com/pablorodriguesb/pollhub/dto/VoteDTO.java
@@ -5,10 +5,6 @@ import lombok.Data;
 
 @Data
 public class VoteDTO {
-
-    @NotNull(message = "ID da enquete é obrigatório")
-    private Long pollId;
-
     @NotNull(message = "ID da opção é obrigatório")
     private Long optionId;
 }
