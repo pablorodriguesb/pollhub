2f236e7da4ed83590e09d60792a4030bb9735cbe
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-11 15:33:37 -0300

    refactor: melhora tratamento global de excecoes

--- src/main/java/com/pablorodriguesb/pollhub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pablorodriguesb/pollhub/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.pablorodriguesb.pollhub.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleUserAlreadyExists(
+            UserAlreadyExistsException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST) // 400
+                .body(errorResponse);
+    }
 }
