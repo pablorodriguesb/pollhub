494653006a64759c726ed281ab8d76e3ed08ed15
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-11 15:35:00 -0300

    refactor: aprimora logica de cadastro e validacao de usuario

--- src/main/java/com/pablorodriguesb/pollhub/service/UserService.java
+++ src/main/java/com/pablorodriguesb/pollhub/service/UserService.java
@@ -1,5 +1,6 @@
 package com.pablorodriguesb.pollhub.service;
 
+import com.pablorodriguesb.pollhub.exception.UserAlreadyExistsException;
 import com.pablorodriguesb.pollhub.model.User;
 import com.pablorodriguesb.pollhub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +25,30 @@ public class UserService {
 
     // realizando cadastro de um novo usuario
     public User registerUser(User user) {
+
+        // verifica campo vazio ou nulo de email
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new IllegalArgumentException("O email não pode ser vazio.");
+        }
+
+        // verifica vazio ou nulo de username
+        if (user.getUsername() == null || user.getUsername().isBlank()) {
+            throw new IllegalArgumentException("O username não pode ser vazio.");
+        }
+
         // verificando se o email ja tem cadastro
         if (userRepository.existsByEmailIgnoreCase(user.getEmail())) {
-            throw new IllegalArgumentException("Email já está em uso.");
+            throw new UserAlreadyExistsException("Email já está em uso.");
         }
 
         // verifica se o username ja tem cadastro
         if (userRepository.existsByUsernameIgnoreCase(user.getUsername())) {
-            throw new IllegalArgumentException("Nome de usuário já está em uso.");
+            throw new UserAlreadyExistsException("Nome de usuário já está em uso.");
+        }
+
+        // verifica vazio ou nulo ao codificar senha
+        if (user.getPassword() == null || user.getPassword().isBlank()) {
+            throw new IllegalArgumentException("A senha não pode ser vazia.");
         }
 
         // gerando hash de senha antes de salvar
