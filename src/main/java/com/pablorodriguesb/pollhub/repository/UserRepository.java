e637bae6317a779d2e5a39b57aa68e054cd03342
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-11 15:34:15 -0300

    refactor: ajusta metodos de consulta no UserRepository

--- src/main/java/com/pablorodriguesb/pollhub/repository/UserRepository.java
+++ src/main/java/com/pablorodriguesb/pollhub/repository/UserRepository.java
@@ -2,12 +2,19 @@ package com.pablorodriguesb.pollhub.repository;
 
 import com.pablorodriguesb.pollhub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailIgnoreCase(String username);
     Optional<User> findByUsernameIgnoreCase(String username);
-    boolean existsByEmailIgnoreCase(String email);
-    boolean existsByUsernameIgnoreCase(String username);
+
+    // consulta otimizada com query pois podem nao usar indices
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE LOWER(u.email) = LOWER(:email)")
+    boolean existsByEmailIgnoreCase(@Param("email") String email);
+
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE LOWER(u.username) = LOWER(:username)")
+    boolean existsByUsernameIgnoreCase(@Param("username") String username);
 }
