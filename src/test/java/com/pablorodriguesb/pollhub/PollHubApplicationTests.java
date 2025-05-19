dfca9a493fd4d0c3cf9bc57ad9522a32604048a3
Pablo Rodrigues <pablo.rodriguesbat@gmail.com>
2025-05-19 11:27:30 -0300

    test: ajustes de configuracao de testes e banco H2 para CI/CD

--- src/test/java/com/pablorodriguesb/pollhub/PollHubApplicationTests.java
+++ src/test/java/com/pablorodriguesb/pollhub/PollHubApplicationTests.java
@@ -2,12 +2,15 @@ package com.pablorodriguesb.pollhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class PollHubApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Contexto carregado com sucesso!");
 	}
 
 }
